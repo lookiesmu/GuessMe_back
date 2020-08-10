011c9338ed9a0d859a2b2f1cf37da87675bf236b
dw <devdw98@gmail.com>
2020-08-10 16:41:17 +0900

    user entity

--- src/main/java/ac/kr/smu/lookie/guessme/domain/User.java
+++ src/main/java/ac/kr/smu/lookie/guessme/domain/User.java
@@ -1,4 +1,28 @@
 package ac.kr.smu.lookie.guessme.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@NoArgsConstructor
+@Getter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_id;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String password;
+
+    @Column
+    @ColumnDefault("0")
+    private int quizCreate;
+
 }
--- src/main/java/ac/kr/smu/lookie/guessme/repository/UserRepository.java
+++ src/main/java/ac/kr/smu/lookie/guessme/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package ac.kr.smu.lookie.guessme.repository;
 
-public class UserRepository {
+import ac.kr.smu.lookie.guessme.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findByNickname(String nickname);
+
+
 }
