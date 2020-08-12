4717e2a770e1a033436e6b83724cafa292d965c2
YouHojoon <dbghwns66@daum.net>
2020-08-13 00:04:50 +0900

    fix:return of login

--- src/main/java/ac/kr/smu/lookie/guessme/controller/LoginController.java
+++ src/main/java/ac/kr/smu/lookie/guessme/controller/LoginController.java
@@ -1,10 +1,11 @@
 package ac.kr.smu.lookie.guessme.controller;
 
 import ac.kr.smu.lookie.guessme.service.LoginService;
-import ac.kr.smu.lookie.guessme.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -20,8 +21,14 @@ public class LoginController {
     private final LoginService loginService;
 
     @PostMapping(value = "/login") //로그인 (+ 에러처리)
-    public ResponseEntity<?> login(@RequestBody Map<String, String> map){
-        return new ResponseEntity<>(loginService.checkLogin(map.get("nickname"), map.get("password")), HttpStatus.OK);
+    public ResponseEntity<?> login(@RequestBody Map<String, String> map) {
+        Map<String, String> body = loginService.checkLogin(map.get("nickname"), map.get("password"));
+        MultiValueMap<String,String> header = new LinkedMultiValueMap<>();
+        if(body.containsKey("nickname")){//만약 로그인 성공이라면 헤더에 토큰 넣어서 반환
+            header.add("X-AUTH-TOKEN", body.get("token"));
+            body.remove("token");
+        }
+        return new ResponseEntity<>(body,header,HttpStatus.OK);
     }
 
 }
