f98fd3fa24533cb5f2a8703d2e0037a1297a113f
dw <devdw98@gmail.com>
2020-08-12 14:01:20 +0900

    user 중복 확인 권한 변경

--- src/main/java/ac/kr/smu/lookie/guessme/controller/LoginController.java
+++ src/main/java/ac/kr/smu/lookie/guessme/controller/LoginController.java
@@ -1,20 +1,14 @@
 package ac.kr.smu.lookie.guessme.controller;
 
-import ac.kr.smu.lookie.guessme.config.security.JwtTokenProvider;
-import ac.kr.smu.lookie.guessme.domain.User;
-import ac.kr.smu.lookie.guessme.exception.CNicknameSigninFailedException;
-import ac.kr.smu.lookie.guessme.repository.UserRepository;
 import ac.kr.smu.lookie.guessme.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import java.util.Collections;
 import java.util.Map;
-import java.util.Optional;
+
 
 @EnableWebMvc
 @RequiredArgsConstructor
@@ -23,7 +17,7 @@ import java.util.Optional;
 public class LoginController {
 
     private final UserService userService;
-    
+
     @PostMapping(value = "/login") //로그인 (+ 에러처리)
     public ResponseEntity<?> login(@RequestBody Map<String, String> map){
         return new ResponseEntity<>(userService.checkLogin(map.get("nickname"), map.get("password")), HttpStatus.OK);
